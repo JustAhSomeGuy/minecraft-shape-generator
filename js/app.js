0573ba853aaab635c9c54aa6a4b4f572b7940b37
Just SomeGuy <justahsomeguyonyt@gmail.com>
2025-03-07 11:30:39 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("shapeCanvas");
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
+    renderer.setSize(600, 600);
+    document.getElementById("shape-container").appendChild(renderer.domElement);
+
+    const geometry = new THREE.SphereGeometry(10, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88, wireframe: true });
+    const sphere = new THREE.Mesh(geometry, material);
+
+    scene.add(sphere);
+    camera.position.z = 30;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        sphere.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+    animate();
+});
